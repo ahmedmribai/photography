98b5fd6dde2cc48c5c0af2e1548fcfca8fe0be97
unknown <ahmedmribai92@gmail.com>
2020-04-01 09:01:09 +0100

    commit 4

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,8 +2,28 @@ import React from "react";
 import "./Navbar.css";
 
 const Navbar2 = () => {
+  document.addEventListener("DOMContentLoaded", function() {
+    // When the event DOMContentLoaded occurs, it is safe to access the DOM
+    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    const myFunction = () => {
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+      } else {
+        navbar.classList.remove("sticky");
+      }
+    };
+
+    // When the user scrolls the page, execute myFunction
+    window.addEventListener("scroll", myFunction);
+
+    // Get the navbar
+    let navbar = document.getElementById("navbar");
+
+    // Get the offset position of the navbar
+    let sticky = navbar.offsetTop;
+  });
   return (
-    <div className="nav">
+    <div className="nav" id="navbar">
       <input type="checkbox" id="nav-check" />
       <div className="nav-header">
         <a href="/" className="nav-title">
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "./Home.css";
 import "../../components/CustomButton/CustomButton.css";
+import SocialIcons from "../../components/SocialIcons/SocialIcons";
 
 const Home = () => {
   return (
-    <div className="container">
-      <h1 classname="title">hahahahahah</h1>
-      <button className="button">contact</button>
+    <div className="background-new">
+      <h1 className="title">
+        PHOTOGRAPHE
+        <br />
+        OUED ZARGA
+      </h1>
+      <button className="button" id="contact-button">
+        contact
+      </button>
+      <SocialIcons />
     </div>
   );
 };
