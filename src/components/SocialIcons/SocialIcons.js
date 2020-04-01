6de6d49d98ad5559173e15b100d94de853584777
unknown <ahmedmribai92@gmail.com>
2020-04-01 18:31:11 +0100

    commit 5

--- src/components/SocialIcons/SocialIcons.js
+++ src/components/SocialIcons/SocialIcons.js
@@ -1,18 +1,41 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faInstagram
+} from "@fortawesome/free-brands-svg-icons";
 
+import "./SocialIcons.css";
 const SocialIcons = () => {
   return (
-    <div class="footer-social-icons">
-    <h4 class="_14">Follow us on</h4>
-    <ul class="social-icons">
-        <li><a href="" class="social-icon"> <i class="fa fa-facebook"></i></a></li>
-        <li><a href="" class="social-icon"> <i class="fa fa-twitter"></i></a></li>
-        <li><a href="" class="social-icon"> <i class="fa fa-rss"></i></a></li>
-        <li><a href="" class="social-icon"> <i class="fa fa-youtube"></i></a></li>
-        <li><a href="" class="social-icon"> <i class="fa fa-linkedin"></i></a></li>
-        <li><a href="" class="social-icon"> <i class="fa fa-github"></i></a></li>
-    </ul>
-</div>
+    <div>
+      <h1 className="social-title">Follow me</h1>
+      <p className="social-box">
+        <a
+          href="https://www.youtube.com/c/jamesqquick"
+          className="youtube social"
+        >
+          <FontAwesomeIcon icon={faYoutube} size="2x" />
+        </a>
+        <a
+          href="https://www.facebook.com/learnbuildteach/"
+          className="facebook social"
+        >
+          <FontAwesomeIcon icon={faFacebook} size="2x" />
+        </a>
+        <a
+          href="http://www.instagram.com/larnbuildteach"
+          className="instagram social"
+        >
+          <FontAwesomeIcon icon={faInstagram} size="2x" />
+        </a>
+        <a href="https://wwww.twitter.com" className="twitter social">
+          <FontAwesomeIcon icon={faTwitter} size="2x" />
+        </a>
+      </p>
+    </div>
   );
 };
 
